93bbe2fe48d7be92c9fd99587876801973e6a4bc
andrestch <andrestuesta_22@outlook.com>
2024-11-20 20:42:46 -0500

    -

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,8 @@ const { Sequelize } = require('sequelize');
 const os = require('os');
 const path = require('path'); 
 
-const ruta = path.join(os.homedir(), 'Documents', 'inventario.db');
+//const ruta = path.join(os.homedir(), 'Documents', 'inventario.db');
+const ruta = path.join(__dirname, 'inventario.db');
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
